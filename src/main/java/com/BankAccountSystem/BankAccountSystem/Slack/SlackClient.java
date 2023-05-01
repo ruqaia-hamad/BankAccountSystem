9b271bc30417d16c41dc328c1c397ee7397fc9ed
Ruqaia Hamed <59163691+ruqaia-hamad@users.noreply.github.com>
2023-05-01 11:55:08 +0400

    ..

--- src/main/java/com/BankAccountSystem/BankAccountSystem/Slack/SlackClient.java
+++ src/main/java/com/BankAccountSystem/BankAccountSystem/Slack/SlackClient.java
@@ -7,7 +7,7 @@ public class SlackClient {
     public String sendMessage(String text) {
 
         return WebClient.create().post()
-                .uri("https://hooks.slack.com/services/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")
+                .uri("")
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(new SlackPayload(text))
                 .retrieve()
