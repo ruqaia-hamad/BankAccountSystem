d7ee012c2128353b6f47f031f70225d7e39ec35e
Ruqaia Hamed <59163691+ruqaia-hamad@users.noreply.github.com>
2023-05-15 10:41:35 +0400

    add security login

--- src/main/java/com/BankAccountSystem/BankAccountSystem/Controllers/AccountController.java
+++ src/main/java/com/BankAccountSystem/BankAccountSystem/Controllers/AccountController.java
@@ -114,6 +114,7 @@ public class AccountController {
     }
 
 
+     @PreAuthorize("hasRole('USER')")
     @GetMapping("/getAccountBalance")
     public double getAccountBalance(@RequestParam Integer accountId) {
         return accountService.getAccountBalance(accountId);
